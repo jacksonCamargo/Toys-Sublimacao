9e897392df1eecf4426f282d7bb16f4e48f0da46
Jackson Tristao <jtristao@iguatemi.com.br>
2024-04-06 14:25:51 -0300

    fix:  inserir section  depoimento de clientes script

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const carousel = document.querySelector('.carousel');
+let rotateValue = 0;
 
-// Função para avançar ou retroceder os slides
-function changeSlide(n) {
-  showSlides(slideIndex += n);
+function rotateCarousel() {
+  rotateValue += 120;
+  carousel.style.transform = `translateX(-${rotateValue}%)`;
 }
 
-// Função para mostrar os slides
-function showSlides(n) {
-  let slides = document.getElementsByClassName("slide");
-  
-  // Voltar ao primeiro slide se chegar ao último
-  if (n > slides.length) {
-    slideIndex = 1;
-  }    
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  
-  // Esconder todos os slides
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  
-  // Mostrar o slide atual
-  slides[slideIndex - 1].style.display = "block";  
-}
-
-// Automatizar a mudança de slide a cada 3 segundos (3000 milissegundos)
-setInterval(function() {
-  changeSlide(1);
-}, 3000);
-
-// ONMOUSE
-function addShadow(element) {
-  element.style.boxShadow = '0px 5px 15px rgba(0, 0, 0, 0.3)';
-}
-
-function removeShadow(element) {
-  element.style.boxShadow = 'none';
-}
+setInterval(rotateCarousel, 3000); // Muda a cada 3 segundos
