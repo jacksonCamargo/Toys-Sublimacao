6a30091b56e58a9592b3d0312e8513cb18bc1710
Jackson Tristao <jtristao@iguatemi.com.br>
2024-04-18 17:23:18 -0300

    fix: ajuste efeitos de carrocal  dos card-depoimentos

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+// const carousel = document.querySelector('.carousel');
+// let rotateValue = 0;
+
+// function rotateCarousel() {
+//   rotateValue += 130;
+//   carousel.style.transform = `translateX(-${rotateValue}%)`;
+ 
+// }
+// setInterval(rotateCarousel, 3000); // Muda a cada 3 segundos
 const carousel = document.querySelector('.carousel');
 let rotateValue = 0;
+const totalItems = document.querySelectorAll('.carousel .card').length;
+let currentItem = 0;
 
 function rotateCarousel() {
-  rotateValue += 130;
+  currentItem++;
+  if (currentItem >= totalItems) {
+    currentItem = 0;
+    rotateValue = 0;
+  }
+  rotateValue += 100 / totalItems;
   carousel.style.transform = `translateX(-${rotateValue}%)`;
 }
 
-setInterval(rotateCarousel, 3000); // Muda a cada 3 segundos
+setInterval(rotateCarousel, 3000);
+
