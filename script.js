779ef360bf1d60b1c1ebc09fdedb3499ec4d983c
Jackson Tristao <jtristao@iguatemi.com.br>
2024-04-22 12:18:28 -0300

    fix: limpei comentarios

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// const carousel = document.querySelector('.carousel');
-// let rotateValue = 0;
-
-// function rotateCarousel() {
-//   rotateValue += 130;
-//   carousel.style.transform = `translateX(-${rotateValue}%)`;
- 
-// }
-// setInterval(rotateCarousel, 3000); // Muda a cada 3 segundos
 const carousel = document.querySelector('.carousel');
 let rotateValue = 0;
 const totalItems = document.querySelectorAll('.carousel .card').length;
